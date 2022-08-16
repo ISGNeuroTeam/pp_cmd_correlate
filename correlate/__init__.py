8c7b8f6219d31cccafd7115b43f75a4799c7d846
sromanov <sromanov@isgneuro.com>
2022-08-16 15:52:27 +0300

    initial commit

---
+++ correlate/__init__.py
@@ -0,0 +1,4 @@
+from .command import CorrelateCommand
+
+
+__all__ = ["CorrelateCommand"]
---
+++ correlate/command.py
@@ -0,0 +1,33 @@
+import pandas as pd
+from scipy.signal import correlate, correlation_lags
+from otlang.sdk.syntax import Keyword, Positional, OTLType
+from pp_exec_env.base_command import BaseCommand, Syntax
+
+
+class CorrelateCommand(BaseCommand):
+    # define syntax of your command here
+    syntax = Syntax(
+        [
+            Positional("first_signal", required=True, otl_type=OTLType.TEXT),
+            Positional("second_signal", required=True, otl_type=OTLType.TEXT),
+        ],
+    )
+    use_timewindow = False  # Does not require time window arguments
+    idempotent = True  # Does not invalidate cache
+
+    def transform(self, df: pd.DataFrame) -> pd.DataFrame:
+        self.log_progress('Start correlate command')
+
+        first_signal_column_name = self.get_arg("first_signal").value
+        second_signal_column_name = self.get_arg("second_signal").value
+
+        signal_1 = df[first_signal_column_name].values
+        signal_2 = df[second_signal_column_name].values
+        correlation = correlate(signal_1, signal_2)
+        lags = correlation_lags(len(signal_1), len(signal_2))
+
+        df = pd.DataFrame({"correlation": correlation,
+                           "lags": lags})
+
+        self.log_progress('First part is complete.', stage=1, total_stages=1)
+        return df
